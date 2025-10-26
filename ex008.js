d9c83a75d1a9e81ced7927b9ee136db12b31bb3d
Vinicius-ribero <viniciusribeirojesus77@gmail.com>
2025-10-25 21:17:20 -0300

    scripts

--- ex008.js
+++
@@ -1,6 +0,0 @@
-var vel = 78.2
-console.log(`A velocidade do seu carro ${vel}km/h`)
-if (vel > 60) { //CONDIÇÃO SIMPLES!
-    console.log(`Você ultrpassou a velocidad permitida. MULTADO!`)
-}
-console.log(`Dirija sempre usando cinto de segurança!`)
