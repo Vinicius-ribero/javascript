9dcb796424f4947e8261728d2da7fd6bfd2af702
Vinicius-ribero <viniciusribeirojesus77@gmail.com>
2025-10-30 16:19:26 -0300

    math

---
+++ exercicios/exercicio08/script.js
@@ -0,0 +1,15 @@
+//math= Objeto embutido que fornece uma coleção de //          propriedades e métodos.
+
+
+var x = 3;
+var y = 2;
+var z;
+
+//z = Math.round(x) arredonda um número para o inteiro mais próximo = 3,21 > 3 arredondou o x para 3
+//z = Math.floor(x) floor : arredonda um número para baixo = 3.99 > 3 
+//z = Math.ceil (x) Ceil :Arredonda um número  para cima = 3.21 > 4
+//z = Math.trunc(x)  trunc: Elimina a Parte Decimal de qualquer Número  3,21 > 3
+
+//z = Math.pow(x,y) Pow: Eleva a potência de dois números = 3 
+
+console.log(z)
