0871964882b789635dc838246ff28a604f7606aa
Vinicius-ribero <viniciusribeirojesus77@gmail.com>
2025-11-01 18:58:33 -0300

    operadores =, == ,===, !=, !==

---
+++ exercicios/exercicio18/script.js
@@ -0,0 +1,44 @@
+// = operador de atribuição
+
+// == operador de comparação (compara se os valores são iguais)
+
+ /*const PI = 3.14 
+
+if (PI === "3.14"){
+     console.log("Este é o PI")
+ }else{
+     console.log("Este não é um PI")
+ }*/ 
+
+
+// === operador de igualdade estrita (compara se os valores e o tipo de dados são iguais)
+
+/*const PI = 3.14 
+
+if (PI === 3.14){
+    console.log("Este é o PI")
+}else{
+    console.log("Este não é um PI")
+}*/
+
+
+// != operador de desigualdade
+
+
+/*const PI = 3.14 
+
+if (PI != "3.14"){
+    console.log("Este não é PI")
+}else{
+    console.log("Este é um PI")
+}*/
+
+// !== operador de desigualdade estrita
+
+const PI = "3.14"
+
+if (PI !== "3.14"){
+    console.log("Este não é PI")
+}else{
+    console.log("Este é um PI")
+}
